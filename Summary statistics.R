d92eb5fe944e4e4487823062ef05ad0e650cc3fa
Anri Sakakibara <anrisakaki@gmail.com>
2025-03-10 18:34:15 +0100

    Summary statistics

--- Summary statistics.R
+++ Summary statistics.R
@@ -1,12 +1,12 @@
 sum_radio_yr <- gdf %>% 
-  group_by(radio, year) %>% 
+  group_by(radio, year_created) %>% 
   filter(year > 1980) %>% 
   st_drop_geometry() %>% 
   summarise(n = n())
 
 sum_radio_cummulative_yr <- gdf %>%
-  group_by(radio, year) %>%
-  filter(year > 1980) %>%
+  group_by(radio, year_created) %>%
+  filter(year_created > 1980) %>%
   st_drop_geometry() %>%
   summarise(n = n(), .groups = "drop") %>%
   arrange(radio, year) %>%
