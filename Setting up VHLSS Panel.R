0d0246e18a785b22aec3f03f53dc3b1fde6a7d78
Anri Sakakibara <anrisakaki@gmail.com>
2025-07-21 17:39:04 +0200

    Setting up VHLSS panel

--- Setting up VHLSS Panel.R
+++ Setting up VHLSS Panel.R
@@ -1,6 +1,4 @@
-# Set up
-
-hhid0810 <- ho1_10
+# HHID
 
 hhid1012 <- ho1_12 %>% 
   filter(ghepho == 1) %>% 
@@ -17,6 +15,7 @@ hhid1012 <- ho1_12 %>%
     diaban12 = diaban,
     hoso12 = hoso
   )
+
 hhid1214 <- ho1_14 %>% 
   select(tinh, huyen, xa, diaban, hoso, tinh12, huyen12, xa12, diaban12, hoso12) %>% 
   filter(if_all(c(tinh12, huyen12, xa12, diaban12, hoso12), ~ !is.na(.))) %>% 
@@ -27,6 +26,7 @@ hhid1214 <- ho1_14 %>%
     diaban14 = diaban,
     hoso14 = hoso
   )
+
 hhid1416 <- ho1_16 %>% 
   select(tinh, huyen, xa, diaban, hoso, tinh14, huyen14, xa14, diaban14, hoso14) %>% 
   filter(if_all(c(tinh14, huyen14, xa14, diaban14, hoso14), ~ !is.na(.))) %>% 
@@ -37,6 +37,7 @@ hhid1416 <- ho1_16 %>%
     diaban16 = diaban,
     hoso16 = hoso
   )
+
 hhid1618 <- ho1_18 %>% 
   select(tinh, huyen, xa, diaban, hoso, tinh16, huyen16, xa16, diaban16, hoso16) %>% 
   filter(if_all(c(tinh16, huyen16, xa16, diaban16, hoso16), ~ !is.na(.))) %>% 
@@ -51,10 +52,64 @@ hhid1618 <- ho1_18 %>%
 hhid1014 <- list(hhid1012, hhid1214) %>% 
   reduce(merge)
 
-hhid1418 <- list(hhid1416, hhid1416, hhid1618) %>% 
-  reduce(merge)
+# IVID
+ivid14 <- m1c_14 %>%
+  select(tinh, huyen, xa, diaban, hoso, matv, m1cc3) %>% 
+  rename(tinh14 = tinh,
+         huyen14 = huyen,
+         xa14 = xa,
+         diaban14 = diaban,
+         hoso14 = hoso,
+         matv14 = matv,
+         matv12 = m1cc3)
+ivid12 <- m1c_12 %>% 
+  select(tinh, huyen, xa, diaban, hoso, m1cc7, m1cc3) %>% 
+  rename(tinh12 = tinh,
+         huyen12 = huyen,
+         xa12 = xa,
+         diaban12 = diaban,
+         hoso12 = hoso,
+         matv12 = m1cc7,
+         matv10 = m1cc3)
 
-vhlss1014 <- bind_rows(vhlss10, vhlss12, vhlss14)
-vhlss1014 <- merge(vhlss1014, panel1014)
-save(vhlss1014, file = ("Clean data/vhlss1014_panel.Rda"))
-write_dta(vhlss1014, "Clean data/vhlss1014_panel.dta")
+ivid1014 <- list(hhid1014, ivid12, ivid14) %>% 
+  reduce(merge) %>% 
+  mutate(ivid = row_number())
+
+ivid10 <- ivid1014 %>% 
+  select(ends_with("10"), ivid) %>% 
+  rename(tinh = tinh10,
+         huyen = huyen10,
+         xa = xa10,
+         diaban = diaban10,
+         hoso = hoso10,
+         matv = matv10) %>% 
+  mutate(year = 2010)
+
+ivid12 <- ivid1014 %>% 
+  select(ends_with("12"), ivid) %>% 
+  rename(tinh = tinh12,
+         huyen = huyen12,
+         xa = xa12,
+         diaban = diaban12,
+         hoso = hoso12,
+         matv = matv12) %>% 
+  mutate(year = 2012)
+
+ivid14 <- ivid1014 %>% 
+  select(ends_with("14"), ivid) %>% 
+  rename(tinh = tinh14,
+         huyen = huyen14,
+         xa = xa14,
+         diaban = diaban14,
+         hoso = hoso14,
+         matv = matv14) %>% 
+  mutate(year = 2014)
+
+ivid1014 <- bind_rows(ivid10, ivid12, ivid14) 
+
+save(ivid1014, file = ("Clean data/vhlss1014_iv_panel.Rda"))
+
+vhlss1014 <- vhlss_all_dist %>% 
+  filter(year > 2009 & year < 2015) %>% 
+  merge(ivid1014)
