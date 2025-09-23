9374957fed542fa321e1c07964196c8b5a7528af
Anri Sakakibara <anrisakaki@gmail.com>
2025-09-23 16:38:50 +0200

    Cleaning LFS

--- Cleaning LFS data.R
+++ Cleaning LFS data.R
@@ -14,24 +14,24 @@ lfs10 <- lfs10 %>%
          yearsworked = c20,
          hours = c22,
          inc = c28,
-        wt = WIEGH_DCTDT) %>%
+         wt = WIEGH_DCTDT) %>%
   mutate(
     female = ifelse(C3 == 2, 1, 0),
     work = ifelse(c10 == 1, 1, 0),
     work2 = ifelse(c21 == 1, 1, 0),
     informal = ifelse(org < 3, 1, 0),
+    formal = ifelse(org > 2, 1, 0),
     agri = ifelse(ind < 50, 1, 0),
     manu = ifelse(ind > 99 & ind < 350, 1, 0),
     service = ifelse(ind > 449, 1, 0),
     nonagri = ifelse(ind > 50, 1, 0),
+    monthint = ifelse(kydt == 1, 4, 10),
     year = 2010
   ) %>%
-  select(year, tinh, dban, hoso, STT, age, female, marst, educattain, work, occ, org, ind, emp,
-         agri, manu, service, nonagri, informal, inc, yearsworked, wt) %>%
-  group_by(tinh, dban, hoso) %>% 
-  mutate(hhid = cur_group_id(),
-         children_u7 = sum(age < 7, na.rm = T),
-         nchild = sum(age < 18, na.rm = T))
+  dplyr::select(year, tinh, dban, hoso, STT, monthint, age, female, marst, educattain, work, occ, org, ind, emp,
+         agri, manu, service, nonagri, formal, informal, inc, yearsworked, wt) %>%
+  left_join(prov_umts_month) %>% 
+  mutate(ttt = (year - first_year) * 12 + (monthint - first_month))
 
 lfs11 <- lfs11 %>% 
   rename(tinh = TINH,
@@ -57,6 +57,7 @@ lfs11 <- lfs11 %>%
     female = ifelse(C3 == 2, 1, 0),
     work = ifelse(C13 == 1, 1, 0),
     work2 = ifelse(C55 == 1, 1, 0),
+    formal = ifelse(org > 2, 1, 0),
     informal = ifelse(org < 3, 1, 0),
     agri = ifelse(ind < 500, 1, 0),
     manu = ifelse(ind > 990 & ind < 3500, 1, 0),
@@ -64,12 +65,10 @@ lfs11 <- lfs11 %>%
     nonagri = ifelse(ind > 500, 1, 0),
     year = 2011
   ) %>% 
-  select(year, tinh, dban, hoso, STT, monthint, age, female, marst, educattain, work, occ, org, ind, emp,
-         agri, manu, service, nonagri, informal, inc, yearsworked, wt, nganhcap2) %>%
-  group_by(tinh, dban, hoso, monthint) %>% 
-  mutate(hhid = cur_group_id(),
-         children_u7 = sum(age < 7, na.rm = T),
-         nchild = sum(age < 18, na.rm = T))
+  dplyr::select(year, tinh, dban, hoso, STT, monthint, age, female, marst, educattain, work, occ, org, ind, emp,
+         agri, manu, service, nonagri, formal, informal, inc, yearsworked, wt, nganhcap2) %>%
+  left_join(prov_umts_month) %>% 
+  mutate(ttt = (year - first_year) * 12 + (monthint - first_month)) 
 
 lfs12 <- lfs12 %>% 
   rename(tinh = TINH,
@@ -93,6 +92,7 @@ lfs12 <- lfs12 %>%
     female = ifelse(C3 == 2, 1, 0),
     work = ifelse(C15 == 1, 1, 0),
     work2 = ifelse(C57 == 1, 1, 0),
+    formal = ifelse(org > 2, 1, 0),
     informal = ifelse(org < 3, 1, 0),
     agri = ifelse(ind < 500, 1, 0),
     manu = ifelse(ind > 990 & ind < 3500, 1, 0),
@@ -101,13 +101,10 @@ lfs12 <- lfs12 %>%
     year = 2012
   ) %>% 
   group_by(tinh, dban) %>%
-  mutate(hhid = cumsum(STT == 1)) %>%
   ungroup() %>%
-  select(year, tinh, dban, hhid, STT, age, female, marst, educattain, work, occ, org, ind, emp,
-         agri, manu, service, nonagri, informal, inc, yearsworked, wt) %>%
-  group_by(hhid) %>% 
-  mutate(children_u7 = sum(age < 7, na.rm = T),
-         nchild = sum(age < 18, na.rm = T))
+  dplyr::select(year, tinh, dban, STT, age, female, marst, educattain, work, occ, org, ind, emp,
+         agri, manu, service, nonagri, formal, informal, inc, yearsworked, wt) %>%
+  left_join(prov_umts_month) 
 
 lfs13 <- lfs13 %>% 
   rename(tinh = TINH,
@@ -134,18 +131,17 @@ lfs13 <- lfs13 %>%
     work = ifelse(C16 == 1, 1, 0),
     work2 = ifelse(C43 == 1, 1, 0),
     informal = ifelse(org < 3, 1, 0),
+    formal = ifelse(org > 2, 1, 0),
     agri = ifelse(ind < 500, 1, 0),
     manu = ifelse(ind > 990 & ind < 3500, 1, 0),
     service = ifelse(ind > 4500, 1, 0),
     nonagri = ifelse(ind > 500, 1, 0),
     year = 2013
   ) %>% 
-  select(year, tinh, dban, hoso, STT, monthint, age, female, marst, educattain, work, occ, org, ind, emp,
-         agri, manu, service, nonagri, informal, inc, yearsworked, wt) %>%
-  group_by(tinh, dban, hoso, monthint) %>% 
-  mutate(hhid = cur_group_id(),
-         children_u7 = sum(age < 7, na.rm = T),
-         nchild = sum(age < 18, na.rm = T))
+  dplyr::select(year, tinh, dban, hoso, STT, monthint, age, female, marst, educattain, work, occ, org, ind, emp,
+         agri, manu, service, nonagri, formal, informal, inc, yearsworked, wt) %>%
+  left_join(prov_umts_month) %>% 
+  mutate(ttt = (year - first_year) * 12 + (monthint - first_month)) 
 
 lfs14 <- lfs14 %>% 
   rename(tinh = TINH,
@@ -172,18 +168,17 @@ lfs14 <- lfs14 %>%
     work = ifelse(C16 == 1, 1, 0),
     work2 = ifelse(C45 < 3, 1, 0),
     informal = ifelse(org < 3, 1, 0),
+    formal = ifelse(org > 2, 1, 0),
     agri = ifelse(ind < 500, 1, 0),
     manu = ifelse(ind > 990 & ind < 3500, 1, 0),
     service = ifelse(ind > 4500, 1, 0),
     nonagri = ifelse(org < 3 & ind > 500, 1, 0),
     year = 2014
   ) %>% 
-  select(year, tinh, dban, hoso, STT, monthint, age, female, marst, educattain, work, occ, org, ind, emp,
-         agri, manu, service, nonagri, informal, inc, yearsworked, wt) %>%
-  group_by(tinh, dban, hoso, monthint) %>% 
-  mutate(hhid = cur_group_id(),
-         children_u7 = sum(age < 7, na.rm = T),
-         nchild = sum(age < 18, na.rm = T))
+  dplyr::select(year, tinh, dban, hoso, STT, monthint, age, female, marst, educattain, work, occ, org, ind, emp,
+         agri, manu, service, nonagri, formal, informal, inc, yearsworked, wt) %>%
+  left_join(prov_umts_month) %>% 
+  mutate(ttt = (year - first_year) * 12 + (monthint - first_month)) 
 
 # HỘ KINH DOANH CÁ THỂ (2014) = CƠ SỞ KD CÁ THỂ (2015)
 # HỘ/CÁ NHÂN (2014) = CÁ NHÂN LÀM TỰ DO (2015) + HỘ NLTS (2015)
@@ -213,18 +208,17 @@ lfs15 <- lfs15 %>%
     female = ifelse(C3 == 2, 1, 0),
     work = ifelse(C14 == 1 | C15 == 1, 1, 0),
     informal = ifelse(org < 4, 1, 0),
+    formal = ifelse(org > 3, 1, 0),
     agri = ifelse(ind < 500, 1, 0),
     manu = ifelse(ind > 990 & ind < 3500, 1, 0),
     service = ifelse(ind > 4500, 1, 0),
     nonagri = ifelse(ind > 500, 1, 0),
     year = 2015
   ) %>% 
-  select(year, tinh, huyen, dban, hoso, STT, monthint, age, female, marst, educattain, work, occ, org, ind, emp,
-         agri, manu, service, nonagri, informal, inc, yearsworked, wt) %>%
-  group_by(tinh, huyen, dban, hoso, monthint) %>% 
-  mutate(hhid = cur_group_id(),
-         children_u7 = sum(age < 7, na.rm = T),
-         nchild = sum(age < 18, na.rm = T))
+  dplyr::select(year, tinh, huyen, dban, hoso, STT, monthint, age, female, marst, educattain, work, occ, org, ind, emp,
+         agri, manu, service, nonagri, formal, informal, inc, yearsworked, wt) %>%
+  left_join(prov_umts_month) %>% 
+  mutate(ttt = (year - first_year) * 12 + (monthint - first_month)) 
 
 lfs16 <- lfs16 %>% 
   rename(tinh = TINH,
@@ -249,18 +243,17 @@ lfs16 <- lfs16 %>%
     female = ifelse(c3 == 2, 1, 0),
     work = ifelse(c14 == 1 | c15 == 1, 1, 0),
     informal = ifelse(org < 4, 1, 0),
+    formal = ifelse(org > 3, 1, 0),
     agri = ifelse(ind < 500, 1, 0),
     manu = ifelse(ind > 990 & ind < 3500, 1, 0),
     service = ifelse(ind > 4500, 1, 0),
     nonagri = ifelse(ind > 500, 1, 0),
     year = 2016
   ) %>% 
-  select(year, tinh, huyen, hoso, STT, monthint, age, female, marst, educattain, work, occ, org, ind, emp,
-         agri, manu, service, nonagri, informal, inc, yearsworked, wt) %>%
-  group_by(tinh, huyen, hoso, monthint) %>% 
-  mutate(hhid = cur_group_id(),
-         children_u7 = sum(age < 7, na.rm = T),
-         nchild = sum(age < 18, na.rm = T))
+  dplyr::select(year, tinh, huyen, hoso, STT, monthint, age, female, marst, educattain, work, occ, org, ind, emp,
+         agri, manu, service, nonagri, formal, informal, inc, yearsworked, wt) %>%
+  left_join(prov_umts_month) %>% 
+  mutate(ttt = (year - first_year) * 12 + (monthint - first_month)) 
 
 lfs17 <- lfs17 %>% 
   rename(tinh = TINH,
@@ -284,18 +277,17 @@ lfs17 <- lfs17 %>%
     female = ifelse(C3 == 2, 1, 0),
     work = ifelse(C16 == 1 | C17 == 1, 1, 0),
     informal = ifelse(org < 4, 1, 0),
+    formal = ifelse(org > 3, 1, 0),
     agri = ifelse(ind < 500, 1, 0),
     manu = ifelse(ind > 990 & ind < 3500, 1, 0),
     service = ifelse(ind > 4500, 1, 0),
     nonagri = ifelse(ind > 500, 1, 0),
     year = 2017
   ) %>% 
-  select(year, tinh, huyen, dban, hoso, STT, monthint, age, female, marst, educattain, work, occ, org, ind, emp,
-         agri, manu, service, nonagri, informal, inc, yearsworked, wt) %>%
-  group_by(tinh, huyen, dban, hoso, monthint) %>% 
-  mutate(hhid = cur_group_id(),
-         children_u7 = sum(age < 7, na.rm = T),
-         nchild = sum(age < 18, na.rm = T))
+  dplyr::select(year, tinh, huyen, dban, hoso, STT, monthint, age, female, marst, educattain, work, occ, org, ind, emp,
+         agri, manu, service, nonagri, formal, informal, inc, yearsworked, wt) %>%
+  left_join(prov_umts_month) %>% 
+  mutate(ttt = (year - first_year) * 12 + (monthint - first_month)) 
 
 lfs18 <- lfs18 %>% 
   rename(tinh = TINH,
@@ -319,18 +311,17 @@ lfs18 <- lfs18 %>%
     female = ifelse(C3 == 2, 1, 0),
     work = ifelse(C21 == 1 | C22 == 1, 1, 0),
     informal = ifelse(org < 4, 1, 0),
+    formal = ifelse(org > 3, 1, 0),
     agri = ifelse(ind < 500, 1, 0),
     manu = ifelse(ind > 990 & ind < 3500, 1, 0),
     service = ifelse(ind > 4500, 1, 0),
     nonagri = ifelse(ind > 500, 1, 0),
     year = 2018
   ) %>% 
-  select(year, tinh, huyen, dban, hoso, STT, monthint, age, female, marst, educattain, work, occ, org, ind, emp,
-         agri, manu, service, nonagri, informal, inc, yearsworked, wt) %>%
-  group_by(tinh, huyen, dban, hoso, monthint) %>% 
-  mutate(hhid = cur_group_id(),
-         children_u7 = sum(age < 7, na.rm = T),
-         nchild = sum(age < 18, na.rm = T))
+  dplyr::select(year, tinh, huyen, dban, hoso, STT, monthint, age, female, marst, educattain, work, occ, org, ind, emp,
+         agri, manu, service, nonagri, formal, informal, inc, yearsworked, wt) %>%
+  left_join(prov_umts_month) %>% 
+  mutate(ttt = (year - first_year) * 12 + (monthint - first_month)) 
 
 lfs19 <- lfs19 %>% 
   rename(tinh = MATINH,
@@ -353,18 +344,17 @@ lfs19 <- lfs19 %>%
     female = ifelse(C03 == 2, 1, 0),
     work = ifelse(C19 == 1 | C20 == 1, 1, 0),
     informal = ifelse(org < 4, 1, 0),
+    formal = ifelse(org > 3, 1, 0),
     agri = ifelse(ind < 500, 1, 0),
     manu = ifelse(ind > 990 & ind < 3500, 1, 0),
     service = ifelse(ind > 4500, 1, 0),
     nonagri = ifelse(ind > 500, 1, 0),
     year = 2019
   ) %>% 
-  select(year, tinh, huyen, dban, hoso, STT, monthint, age, female, marst, educattain, work, occ, org, ind, emp,
-         agri, manu, service, nonagri, informal, inc, yearsworked, wt)  %>%
-  group_by(tinh, huyen, dban, hoso, monthint) %>% 
-  mutate(hhid = cur_group_id(),
-         children_u7 = sum(age < 7, na.rm = T),
-         nchild = sum(age < 18, na.rm = T))
+  dplyr::select(year, tinh, huyen, dban, hoso, STT, monthint, age, female, marst, educattain, work, occ, org, ind, emp,
+         agri, manu, service, nonagri, formal, informal, inc, yearsworked, wt) %>%
+  left_join(prov_umts_month) %>% 
+  mutate(ttt = (year - first_year) * 12 + (monthint - first_month)) 
 
 lfs20 <- lfs20 %>% 
   rename(tinh = MATINH,
@@ -387,46 +377,50 @@ lfs20 <- lfs20 %>%
     female = ifelse(C03 == 2, 1, 0),
     work = ifelse(C21 == 1 | C20 == 1, 1, 0),
     informal = ifelse(org < 4, 1, 0),
+    formal = ifelse(org > 3, 1, 0),
     agri = ifelse(ind < 500, 1, 0),
     manu = ifelse(ind > 990 & ind < 3500, 1, 0),
     service = ifelse(ind > 4500, 1, 0),
     nonagri = ifelse(ind > 500, 1, 0),
     year = 2020
   ) %>% 
-  select(year, tinh, huyen, dban, hoso, STT, monthint, age, female, marst, educattain, work, occ, org, ind, emp,
-         agri, manu, service, nonagri, informal, inc, yearsworked, wt) %>%
-  group_by(tinh, huyen, dban, hoso, monthint) %>% 
-  mutate(hhid = cur_group_id(),
-         children_u7 = sum(age < 7, na.rm = T),
-         nchild = sum(age < 18, na.rm = T))
+  dplyr::select(year, tinh, huyen, dban, hoso, STT, monthint, age, female, marst, educattain, work, occ, org, ind, emp,
+         agri, manu, service, nonagri, formal, informal, inc, yearsworked, wt) %>%
+  left_join(prov_umts_month) %>% 
+  mutate(ttt = (year - first_year) * 12 + (monthint - first_month)) 
 
 lfs_all <- bind_rows(lfs10, lfs11, lfs12, lfs13, lfs14, lfs15, lfs16, lfs17, lfs18, lfs19, lfs20) %>% 
   mutate(informal = ifelse(work == 0, NA, informal),
          agri_informal = agri*informal,
-         manu_informal = manu*informal,
-         service_informal = service*informal,
-         nonagri_informal = nonagri*informal) %>% 
-  left_join(umts_coverage, by = c("year", "tinh")) %>% 
-  mutate(coverage = ifelse(coverage_share > 0, 1, 0))
+         manu_formal = manu*formal,
+         service_formal = service*formal,
+         nonagri_formal = nonagri*formal,
+         ytt = floor(ttt/12)) 
 
 lfs_sum_fn <- function(i){
   i %>% 
     summarise(
       work = weighted.mean(work, wt, na.rm = T),
       informal = weighted.mean(informal, wt, na.rm = T),
+      formal = weighted.mean(formal, wt, na.rm = T),
       agri_informal = weighted.mean(agri_informal, wt, na.rm = T),
-      manu_informal = weighted.mean(manu_informal, wt, na.rm = T),
-      service_informal = weighted.mean(service_informal, wt, na.rm = T),
-      nonagri_informal = weighted.mean(nonagri_informal, wt, na.rm = T)
+      manu_formal = weighted.mean(manu_formal, wt, na.rm = T),
+      service_formal = weighted.mean(service_formal, wt, na.rm = T),
+      nonagri_formal = weighted.mean(nonagri_formal, wt, na.rm = T)
     )
 }
 
-lfs_sum <- lfs_all %>% 
+lfs_sum_prov<- lfs_all %>% 
   filter(age > 19 & age < 65) %>% 
-  group_by(year) %>% 
-  lfs_sum_fn()
+  group_by(ytt, ttt, tinh, year) %>% 
+  lfs_sum_fn() %>% 
+  group_by(tinh, year) %>% 
+  mutate(prov_year = cur_group_id()) %>% 
+  ungroup()
 
 lfs_sum_f <- lfs_all %>% 
   filter(female == 1 & age > 19 & age < 65) %>% 
   group_by(year) %>% 
   lfs_sum_fn()
+
+save(lfs_sum_prov, file = "Clean data/lfs_sum_prov.Rda")
