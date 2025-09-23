b1d9ba82203de9f33c55661f624772fb9730c904
Anri Sakakibara <anrisakaki@gmail.com>
2025-09-23 21:44:59 +0200

    District-level CB data

--- Cleaning CB data.R
+++ Cleaning CB data.R
@@ -1,3 +1,7 @@
+################################
+# DISTRICT - LEVEL 3G COVERAGE #
+################################
+
 # Consistent district codes 
 
 dist18 <- dist9919 %>%
@@ -99,6 +103,8 @@ vnmap2 <- st_transform(vnmap2, st_crs(cb13_int)) %>%
 vnmap2$dist_area <- as.numeric(st_area(vnmap2))
 dist_area <- vnmap2 %>% st_drop_geometry() %>% dplyr::select(GID_2, NAME_1, NAME_2, dist_area)
 
+# Collins Bartholomew data 
+
 cb13_int$area_m2 <- as.numeric(st_area(cb13_int))
 cb14_int$area_m2 <- as.numeric(st_area(cb14_int)) 
 cb15_int$area_m2 <- as.numeric(st_area(cb15_int)) 
@@ -142,114 +148,9 @@ write_dta(cb_3G_1320, "Clean data/cb_3G_1320.dta")
 save(cb_3G_vhlss, file = "Clean data/cb_3G_vhlss.Rda")
 write_dta(cb_3G_vhlss, "Clean data/cb_3G_vhlss.dta")
 
-# Province 
-vnmap1 <- st_transform(vnmap1, st_crs(cb13_int_prov))
-vnmap1$prov_area <- as.numeric(st_area(vnmap1))
-prov_area <- vnmap1 %>% st_drop_geometry() %>% dplyr::select(GID_1, NAME_1, prov_area)
-
-cb13_int_prov$area_m2 <- as.numeric(st_area(cb13_int_prov))
-cb14_int_prov$area_m2 <- as.numeric(st_area(cb14_int_prov)) 
-cb15_int_prov$area_m2 <- as.numeric(st_area(cb15_int_prov)) 
-cb16_int_prov$area_m2 <- as.numeric(st_area(cb16_int_prov)) 
-cb17_int_prov$area_m2 <- as.numeric(st_area(cb17_int_prov)) 
-cb18_int_prov$area_m2 <- as.numeric(st_area(cb18_int_prov)) 
-cb19_int_prov$area_m2 <- as.numeric(st_area(cb19_int_prov)) 
-cb20_int_prov$area_m2 <- as.numeric(st_area(cb20_int_prov)) 
-
-cb_prov_cov_fn <- function(i) {
-  i %>% 
-    st_drop_geometry() %>% 
-    group_by(GID_1) %>% 
-    summarise(prov_cov = sum(area_m2), .groups = "drop") %>% 
-    left_join(prov_area) %>% 
-    mutate(prov_cov_share = prov_cov/prov_area,
-           prov_cov_round = round(prov_cov_share, 2))
-}
-
-cb13_cov_prov <- cb13_int_prov %>% cb_prov_cov_fn() %>% mutate(year = 2013)
-cb14_cov_prov <- cb14_int_prov %>% cb_prov_cov_fn() %>% mutate(year = 2014)
-cb15_cov_prov <- cb15_int_prov %>% cb_prov_cov_fn() %>% mutate(year = 2015)
-cb16_cov_prov <- cb16_int_prov %>% cb_prov_cov_fn() %>% mutate(year = 2016)
-cb17_cov_prov <- cb17_int_prov %>% cb_prov_cov_fn() %>% mutate(year = 2017)
-cb18_cov_prov <- cb18_int_prov %>% cb_prov_cov_fn() %>% mutate(year = 2018)
-cb19_cov_prov <- cb19_int_prov %>% cb_prov_cov_fn() %>% mutate(year = 2019)
-cb20_cov_prov <- cb20_int_prov %>% cb_prov_cov_fn() %>% mutate(year = 2020)
-
-cb_3G_prov_1320 <- bind_rows(cb13_cov_prov, cb14_cov_prov, cb15_cov_prov, cb16_cov_prov,
-                             cb17_cov_prov, cb18_cov_prov, cb19_cov_prov, cb20_cov_prov) %>% 
-  dplyr::select(-c(GID_1, prov_cov, prov_area)) %>% 
-  mutate(tinh = recode(NAME_1,
-                       'An Giang' = 89,
-                       'Bà Rịa - Vũng Tàu' = 77,
-                       'Bắc Giang' = 24,
-                       'Bắc Kạn' = 6,
-                       'Bạc Liêu' = 95,
-                       'Bắc Ninh' = 27,
-                       'Bến Tre' = 83,
-                       'Bình Định' = 52,
-                       'Bình Dương' = 74,
-                       'Bình Phước' = 70,
-                       'Bình Thuận' = 60,
-                       'Cà Mau' = 96,
-                       'Cần Thơ' = 92,
-                       'Cao Bằng' = 4,
-                       'Đà Nẵng' = 48,
-                       'Đắk Lắk' = 66,
-                       'Đắk Nông' = 67,
-                       'Điện Biên' = 11,
-                       'Đồng Nai' = 75,
-                       'Đồng Tháp' = 87,
-                       'Gia Lai' = 64,
-                       'Hà Giang' = 2,
-                       'Hà Nam' = 35,
-                       'Hà Nội' = 1,
-                       'Hà Tĩnh' = 42,
-                       'Hải Dương' = 30,
-                       'Hải Phòng' = 31,
-                       'Hậu Giang' = 93,
-                       'Hồ Chí Minh' = 79,
-                       'Hoà Bình' = 17,
-                       'Hưng Yên' = 33,
-                       'Khánh Hòa' = 56,
-                       'Kiên Giang' = 91,
-                       'Kon Tum' = 62,
-                       'Lai Châu' = 12,
-                       'Lâm Đồng' = 68,
-                       'Lạng Sơn' = 20,
-                       'Lào Cai' = 10,
-                       'Long An' = 80,
-                       'Nam Định' = 36,
-                       'Nghệ An' = 40,
-                       'Ninh Bình' = 37,
-                       'Ninh Thuận' = 58,
-                       'Phú Thọ' = 25,
-                       'Phú Yên' = 54,
-                       'Quảng Bình' = 44,
-                       'Quảng Nam' = 49,
-                       'Quảng Ngãi' = 51,
-                       'Quảng Ninh' = 22,
-                       'Quảng Trị' = 45,
-                       'Sóc Trăng' = 94,
-                       'Sơn La' = 14,
-                       'Tây Ninh' = 72,
-                       'Thái Bình' = 34,
-                       'Thái Nguyên' = 19,
-                       'Thanh Hóa' = 38,
-                       'Thừa Thiên Huế' = 46,
-                       'Tiền Giang' = 82,
-                       'Trà Vinh' = 84,
-                       'Tuyên Quang' = 8,
-                       'Vĩnh Long' = 86,
-                       'Vĩnh Phúc' = 26,
-                       'Yên Bái' = 15,
-                       .default = NA_real_)) %>% 
-  dplyr::select(-NAME_1) %>% 
-  dplyr::select(tinh, everything())
-
-save(cb_3G_prov_1320, file = "Clean data/cb_3G_prov_1320.Rda")
-write_dta(cb_3G_prov_1320, "Clean data/cb_3G_prov_1320.dta")
-
-# Maps 
+########
+# MAPS #
+########
 
 ggplot() +
   geom_sf(data = cb13, aes(fill = as.factor(DN)), color = NA) +  
