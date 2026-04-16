e5623d4d8d212650c2cdeea71a9d7475b2e9fad4
Anri Sakakibara <anrisakaki@gmail.com>
2026-04-16 17:25:26 +0200

    Cleaning OCI and CB data

--- Cleaning OCI and CB data - district level.R
+++ Cleaning OCI and CB data - district level.R
@@ -272,77 +272,113 @@ umts <- st_join(umts, vnmap2[c("NAME_1", "NAME_2", "ID_2", "urban")]) %>%
 
 proj_crs <- st_crs(32648)
 
-compute_3G_coverage_dist <- function(umts, vnmap2, year_range, proj_crs) {
-  
+compute_3G_coverage_dist <- function(umts, vnmap2, year_range, proj_crs, pop_raster = NULL) {
+
   dist_filtered <- umts %>%
     filter(year_created >= year_range[1] & year_created <= year_range[2])
-  
+
   dist_filtered <- dist_filtered %>%
     mutate(coverage_area = map2(geometry, range, ~ st_make_valid(st_buffer(.x, dist = as.numeric(.y)))))
-  
+
   coverage_sf <- st_sf(geometry = st_sfc(dist_filtered$coverage_area, crs = proj_crs))
-  
+
   merged_coverage <- coverage_sf %>%
     st_union() %>%
     st_sf(geometry = .) %>%
     st_make_valid()
-  
-  dist_cov <- st_intersection(vnmap2, merged_coverage) %>%
+
+  dist_cov_sf <- st_intersection(vnmap2, merged_coverage) %>% st_make_valid()
+
+  dist_cov <- dist_cov_sf %>%
     mutate(
-      coverage_area = st_area(geometry), 
-      coverage_area_m2 = as.numeric(coverage_area),  
-      share_3G_OCI = coverage_area_m2 / Shape_Area  
+      coverage_area = st_area(geometry),
+      coverage_area_m2 = as.numeric(coverage_area),
+      share_3G_OCI = coverage_area_m2 / Shape_Area
     ) %>%
-    st_drop_geometry() %>%  
-    dplyr::select(ID_2, share_3G_OCI) 
-  
+    st_drop_geometry() %>%
+    dplyr::select(ID_2, share_3G_OCI)
+
+  if (!is.null(pop_raster)) {
+    pop_proj <- terra::project(pop_raster, terra::crs(dist_cov_sf))
+    pop_proj <- terra::aggregate(pop_proj, fact = 3, fun = "sum", na.rm = TRUE)
+
+    pop_covered_vals <- exact_extract(pop_proj, dist_cov_sf, fun = "sum")
+    pop_covered <- dist_cov_sf %>%
+      st_drop_geometry() %>%
+      ungroup() %>%
+      dplyr::select(ID_2) %>%
+      mutate(pop_covered = pop_covered_vals) %>%
+      group_by(ID_2) %>%
+      summarise(pop_covered = sum(pop_covered, na.rm = TRUE), .groups = "drop")
+
+    pop_total_vals <- exact_extract(pop_proj, vnmap2, fun = "sum")
+    pop_total <- vnmap2 %>%
+      st_drop_geometry() %>%
+      ungroup() %>%
+      dplyr::select(ID_2) %>%
+      mutate(total_pop = pop_total_vals)
+
+    dist_cov <- dist_cov %>%
+      left_join(pop_covered, by = "ID_2") %>%
+      left_join(pop_total, by = "ID_2") %>%
+      mutate(
+        pop_covered = replace_na(pop_covered, 0),
+        ppn_3G_OCI = pop_covered / total_pop
+      ) %>%
+      dplyr::select(ID_2, share_3G_OCI, ppn_3G_OCI)
+  }
+
   return(dist_cov)
 }
 
 dists <- vnmap2 %>% st_drop_geometry() %>% ungroup() %>% select(ID_2) %>% distinct()
 
 build_dist_cov_year <- function(year_end, umts, vnmap2, dists, proj_crs) {
-  compute_3G_coverage_dist(umts, vnmap2, c(2010, year_end), proj_crs) %>%
+  pop_raster <- if (year_end <= 2017) pop_list[[paste0("pop", substr(as.character(year_end), 3, 4))]] else NULL
+  compute_3G_coverage_dist(umts, vnmap2, c(2010, year_end), proj_crs, pop_raster = pop_raster) %>%
     full_join(dists, by = "ID_2") %>%
     mutate(year = year_end)
 }
 
 assign_oci_treatment <- function(df, mean_cutoff, med_cutoff) {
   df %>%
-    select(year, ID_2, share_3G_OCI) %>%
-    mutate(share_3G_OCI = coalesce(share_3G_OCI, 0)) %>%
+    select(year, ID_2, share_3G_OCI, any_of("ppn_3G_OCI")) %>%
+    mutate(share_3G_OCI = coalesce(share_3G_OCI, 0),
+           ppn_3G_OCI   = coalesce(ppn_3G_OCI, 0)) %>%
     group_by(ID_2) %>%
     mutate(
-      mean_3G_OCI = ifelse(any(share_3G_OCI >= mean_cutoff), 1, 0),
-      med_3G_OCI = ifelse(any(share_3G_OCI >= med_cutoff), 1, 0),
-      year_mean_OCI = ifelse(any(share_3G_OCI >= mean_cutoff),
-                             min(year[share_3G_OCI >= mean_cutoff], na.rm = TRUE),
+      mean_3G_OCI = ifelse(any(ppn_3G_OCI >= mean_cutoff), 1, 0),
+      med_3G_OCI  = ifelse(any(ppn_3G_OCI >= med_cutoff), 1, 0),
+      year_mean_OCI = ifelse(any(ppn_3G_OCI >= mean_cutoff),
+                             min(year[ppn_3G_OCI >= mean_cutoff], na.rm = TRUE),
                              NA),
-      year_med_OCI = ifelse(any(share_3G_OCI >= med_cutoff),
-                            min(year[share_3G_OCI >= med_cutoff], na.rm = TRUE),
-                            NA),
-      year_OCI = ifelse(any(share_3G_OCI > 0),
-                        min(year[share_3G_OCI > 0], na.rm = TRUE),
+      year_med_OCI  = ifelse(any(ppn_3G_OCI >= med_cutoff),
+                             min(year[ppn_3G_OCI >= med_cutoff], na.rm = TRUE),
+                             NA),
+      year_OCI = ifelse(any(ppn_3G_OCI > 0),
+                        min(year[ppn_3G_OCI > 0], na.rm = TRUE),
                         NA)
     )
 }
 
 dist_cov_yearly <- map_dfr(2010:2018, build_dist_cov_year, umts = umts, vnmap2 = vnmap2, dists = dists, proj_crs = proj_crs)
 
-mean_coverage17_oci <- dist_cov_yearly %>%
-  filter(year == 2017) %>%
-  mutate(share_3G_OCI = coalesce(share_3G_OCI, 0)) %>%
-  summarise(mean_share_3G_OCI = mean(share_3G_OCI)) %>%
-  pull(mean_share_3G_OCI)
 median_coverage17_oci <- dist_cov_yearly %>%
   filter(year == 2017) %>%
-  mutate(share_3G_OCI = coalesce(share_3G_OCI, 0)) %>%
-  summarise(med_share_3G_OCI = median(share_3G_OCI)) %>%
-  pull(med_share_3G_OCI)
+  mutate(ppn_3G_OCI = coalesce(ppn_3G_OCI, 0)) %>%
+  summarise(ppn_3G_OCI = median(ppn_3G_OCI)) %>%
+  pull(ppn_3G_OCI)
+
+mean_coverage17_oci <- dist_cov_yearly %>%
+  filter(year == 2017) %>%
+  mutate(ppn_3G_OCI = coalesce(ppn_3G_OCI, 0)) %>%
+  summarise(ppn_3G_OCI = mean(ppn_3G_OCI)) %>%
+  pull(ppn_3G_OCI)
 
 oci_dist_1017 <- dist_cov_yearly %>%
   filter(year <= 2017) %>%
   assign_oci_treatment(mean_cutoff = mean_coverage17_oci, med_cutoff = median_coverage17_oci)
+
 save(oci_dist_1017, file = "Clean data/oci_dist_1017.Rda")
 write_dta(oci_dist_1017, "Clean data/oci_dist_1017.dta")
 
@@ -352,8 +388,8 @@ write_dta(oci_dist_1017, "Clean data/oci_dist_1017.dta")
 
 vnmap2 <- st_transform(vnmap2, crs = crs(cb13))
 
-raster_list <- list(cb12, cb13, cb14, cb15, cb16, cb17, cb18)
-names(raster_list) <- paste0("share_3G_", 12:18)
+raster_list <- list(cb12, cb13, cb14, cb15, cb16, cb17)
+names(raster_list) <- paste0("share_3G_", 12:17)
 
 shares <- map_dfc(
   raster_list,
@@ -364,54 +400,83 @@ shares <- map_dfc(
   })
 )
 
-cb_1218_shp <- bind_cols(vnmap2, shares)
+cb_1217_shp <- bind_cols(vnmap2, shares)
 
-cb_dist_1218 <- cb_1218_shp %>%
+cb_dist_1217 <- cb_1217_shp %>%
   st_drop_geometry() %>%
   select(ID_2, starts_with("share_"))
 
-build_cb_year <- function(year_value, cb_dist_1218) {
+build_cb_year <- function(year_value, cb_dist_1217, vnmap2) {
   if (year_value < 2012) {
-    return(cb_dist_1218 %>% select(ID_2) %>% mutate(year = year_value, share_3G_CB = 0))
+    return(cb_dist_1217 %>% select(ID_2) %>% mutate(year = year_value, share_3G_CB = 0, ppn_3G_CB = 0))
   }
 
-  source_col <- ifelse(year_value <= 2015, paste0("share_3G_", substr(year_value, 3, 4)), "share_3G_15")
-  cb_dist_1218 %>%
+  source_col <- paste0("share_3G_", substr(year_value, 3, 4))
+  result <- cb_dist_1217 %>%
     select(ID_2, all_of(source_col)) %>%
     mutate(year = year_value) %>%
     rename(share_3G_CB = all_of(source_col))
+
+  cb_ras   <- raster_list[[paste0("share_3G_", substr(as.character(year_value), 3, 4))]]
+  pop_proj <- terra::resample(
+    terra::project(pop_list[[paste0("pop", substr(as.character(year_value), 3, 4))]], terra::crs(cb_ras)),
+    cb_ras,
+    method = "sum"
+  )
+  vals <- exact_extract(cb_ras, vnmap2,
+    function(values, coverage_fraction, weights) {
+      covered <- ifelse(is.na(values), 0, as.numeric(values > 0))
+      pop_cov <- sum(covered * weights * coverage_fraction, na.rm = TRUE)
+      pop_tot <- sum(weights * coverage_fraction, na.rm = TRUE)
+      ifelse(pop_tot > 0, pop_cov / pop_tot, 0)
+    }, weights = pop_proj)
+
+  pop_df <- vnmap2 %>%
+    st_drop_geometry() %>%
+    ungroup() %>%
+    dplyr::select(ID_2) %>%
+    mutate(ppn_3G_CB = vals)
+
+  result %>% left_join(pop_df, by = "ID_2")
 }
 
 assign_cb_treatment <- function(df, mean_cutoff, med_cutoff) {
   df %>%
     group_by(ID_2) %>%
     mutate(
-      mean_3G_CB = ifelse(any(share_3G_CB >= mean_cutoff), 1, 0),
-      med_3G_CB = ifelse(any(share_3G_CB >= med_cutoff), 1, 0),
-      year_mean_CB = ifelse(any(share_3G_CB >= mean_cutoff),
-                            min(year[share_3G_CB >= mean_cutoff], na.rm = TRUE),
+      mean_3G_CB = ifelse(any(ppn_3G_CB >= mean_cutoff, na.rm = TRUE), 1, 0),
+      med_3G_CB  = ifelse(any(ppn_3G_CB >= med_cutoff, na.rm = TRUE), 1, 0),
+      year_mean_CB = ifelse(any(ppn_3G_CB >= mean_cutoff, na.rm = TRUE),
+                            min(year[ppn_3G_CB >= mean_cutoff], na.rm = TRUE),
                             NA),
-      year_med_CB = ifelse(any(share_3G_CB >= med_cutoff),
-                           min(year[share_3G_CB >= med_cutoff], na.rm = TRUE),
-                           NA),
-      share_3G_CB = ifelse(year < 2012, NA, share_3G_CB)
+      year_med_CB  = ifelse(any(ppn_3G_CB >= med_cutoff, na.rm = TRUE),
+                            min(year[ppn_3G_CB >= med_cutoff], na.rm = TRUE),
+                            NA),
+      share_3G_CB = ifelse(year < 2012, NA, share_3G_CB),
+      ppn_3G_CB   = ifelse(year < 2012, NA, ppn_3G_CB)
     )
 }
 
-cb_dist_yearly <- map_dfr(2010:2018, build_cb_year, cb_dist_1218 = cb_dist_1218)
+cb_dist_yearly <- map_dfr(2010:2017, build_cb_year, cb_dist_1217 = cb_dist_1217, vnmap2 = vnmap2) %>%
+  group_by(ID_2) %>%
+  arrange(year) %>%
+  mutate(share_3G_CB = cummax(replace_na(share_3G_CB, 0)),
+         ppn_3G_CB   = cummax(replace_na(ppn_3G_CB, 0))) %>%
+  ungroup()
 
 mean_coverage17_cb <- cb_dist_yearly %>%
   filter(year == 2017) %>%
-  summarise(mean_share_3G_CB = mean(share_3G_CB, na.rm = TRUE)) %>%
-  pull(mean_share_3G_CB)
+  summarise(mean_ppn_3G_CB  = mean(ppn_3G_CB , na.rm = TRUE)) %>%
+  pull(mean_ppn_3G_CB)
 median_coverage17_cb <- cb_dist_yearly %>%
   filter(year == 2017) %>%
-  summarise(med_share_3G_CB = median(share_3G_CB, na.rm = TRUE)) %>%
-  pull(med_share_3G_CB)
+  summarise(med_ppn_3G_CB = median(ppn_3G_CB, na.rm = TRUE)) %>%
+  pull(med_ppn_3G_CB)
 
 cb_dist_1017 <- cb_dist_yearly %>%
   filter(year <= 2017) %>%
-  assign_cb_treatment(mean_cutoff = mean_coverage17_cb, med_cutoff = median_coverage17_cb)
+  assign_cb_treatment(mean_cutoff = mean_coverage17_cb, med_cutoff = median_coverage17_cb) %>%
+  ungroup()
 
 save(cb_dist_1017, file = "Clean data/cb_dist_1017.Rda")
 write_dta(cb_dist_1017, "Clean data/cb_dist_1017.dta")
@@ -546,14 +611,14 @@ ggplot() +
     ),
     labels = c(
       "2010" = "2010 (N = 4)",
-      "2011" = "2011 (N = 13)",
-      "2012" = "2012 (N = 48)",
-      "2013" = "2013 (N = 117)",
-      "2014" = "2014 (N = 48)",
-      "2015" = "2015 (N = 182)",
-      "2016" = "2016 (N = 58)",
-      "2017" = "2017 (N = 20)",
-      "Control" = " Control (N = 172)")
+      "2011" = "2011 (N = 3)",
+      "2012" = "2012 (N = 51)",
+      "2013" = "2013 (N = 92)",
+      "2014" = "2014 (N = 53)",
+      "2015" = "2015 (N = 179)",
+      "2016" = "2016 (N = 56)",
+      "2017" = "2017 (N = 22)",
+      "Control" = " Control (N = 191)")
   )+
   geom_sf(data = vnmap2, fill = NA, color = "black", size = 0.2) +
   labs(fill = "Year treated") +
@@ -572,7 +637,7 @@ ggplot() +
     axis.ticks   = element_blank(),
     axis.line    = element_blank()
   )
-ggsave("C:/Users/Anri Sakakibara/Dropbox/Apps/Overleaf/3G in Vietnam/Figures/Descriptive Stats/mean_OCI_map.png", width = 7, height = 14)
+ggsave("C:/Users/Anri Sakakibara/Dropbox/Apps/Overleaf/3G in Vietnam/Figures/Descriptive Stats/ppn_mean_OCI_map.png", width = 7, height = 14)
 
 ggplot(sum_year_3G, aes(x = year)) +
   geom_line(aes(y = mean, color = "Mean"), linewidth = 1) +
@@ -598,10 +663,10 @@ ggplot(sum_year_3G, aes(x = year)) +
     axis.ticks = element_line(color = "black")
   ) +
   scale_x_continuous(breaks = 2010:2017)
-ggsave("C:/Users/Anri Sakakibara/Dropbox/Apps/Overleaf/3G in Vietnam/Figures/Descriptive Stats/cov_yearly_OCI.png", width = 7, height = 7)
+ggsave("C:/Users/Anri Sakakibara/Dropbox/Apps/Overleaf/3G in Vietnam/Figures/Descriptive Stats/cov_ppn_yearly_OCI.png", width = 7, height = 7)
 
 ggplot(
-  dist_3G, aes(x = share_3G_OCI * 100)
+  dist_3G, aes(x = ppn_3G_OCI * 100)
 ) +
   geom_histogram(
     binwidth = 5,
@@ -615,7 +680,7 @@ ggplot(
     breaks = seq(0, 100, 10)
   ) +
   labs(
-    x = "District level 3G Coverage (%)",
+    x = "Share of district population covered by 3G (%)",
     y = "Frequency"
   ) +
   theme_minimal() +
@@ -632,4 +697,4 @@ ggplot(
     axis.ticks = element_line(color = "black"),
     strip.text = element_text(face = "bold", size = 14)
   )
-ggsave("C:/Users/Anri Sakakibara/Dropbox/Apps/Overleaf/3G in Vietnam/Figures/Descriptive Stats/cov_yearly_hist_OCI.png", width = 7, height = 21)
+ggsave("C:/Users/Anri Sakakibara/Dropbox/Apps/Overleaf/3G in Vietnam/Figures/Descriptive Stats/ppn_cov_yearly_hist_OCI.png", width = 7, height = 21)
