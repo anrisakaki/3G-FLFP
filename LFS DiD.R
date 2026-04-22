c54202c181a82baa70283a6a9af835ccb3a25521
Anri Sakakibara <anrisakaki@gmail.com>
2026-04-22 10:22:04 +0200

    LFS DiD

--- LFS DiD.R
+++ LFS DiD.R
@@ -3,7 +3,15 @@ load("Clean data/lfs_sum_dist_m.Rda")
 load("Clean data/lfs_sum_dist_f.Rda")
 
 colours <- c("#4D4D4D", "#1B9E77")
-setFixest_coefplot(grid = F, zero.par = list(type = "dotted", lty = 2), main = "", ref.line = -1, col = c("#4D4D4D", "#1B9E77"))
+setFixest_coefplot(
+  grid = F,
+  zero.par = list(type = "dotted", lty = 2),
+  main = "",
+  ref.line = -1,
+  col = c("#4D4D4D", "#1B9E77"),
+  pt.join.par = list(lwd = 2),
+  lwd = 2
+)
 setFixest_ssc(ssc(adj = FALSE, cluster.adj = FALSE))
 dict = c("share_3G_OCI" = "3G Coverage",
          "work" = "LFP",
@@ -34,8 +42,8 @@ run_twfe_table <- function(df, outcomes, out_file) {
 }
 
 plot_event_study <- function(df_twfe, df_sunab, outcome, out_file) {
-  fml_twfe <- as.formula(paste0(outcome, " ~ i(ytt_mean_OCI, mean_3G_OCI, ref = c(-1, -1000)) + lnexport_all | ID_2 + year"))
-  fml_sunab <- as.formula(paste0(outcome, " ~ sunab(year_mean_OCI, year) + lnexport_all | ID_2 + year"))
+  fml_twfe <- as.formula(paste0(outcome, " ~ i(ytt_mean_OCI, mean_3G_OCI, ref = c(-1, -1000)) + lnexport_all + i(year, sh_manu_exposed_09, ref = 2010) | ID_2 + year"))
+  fml_sunab <- as.formula(paste0(outcome, " ~ sunab(year_mean_OCI, year) + lnexport_all + i(year, sh_manu_exposed_09, ref = 2010) | ID_2 + year"))
 
   jpeg(out_file)
   iplot(
@@ -78,6 +86,12 @@ plot_event_study(default_all, lfs_sum_dist, "agri", file.path(fig_dir, "agri_mea
 plot_event_study(default_all, lfs_sum_dist, "manu", file.path(fig_dir, "manu_mean_OCI.jpeg"))
 plot_event_study(default_all, lfs_sum_dist, "service", file.path(fig_dir, "service_mean_OCI.jpeg"))
 plot_event_study(default_all, lfs_sum_dist, "hhbus", file.path(fig_dir, "hhbus_mean_OCI.jpeg"))
+plot_event_study(subset(lfs_sum_dist, ytt_mean_OCI > -7 & ytt_mean_OCI < 3 | ytt_mean_OCI == -1000),
+                 subset(lfs_sum_dist, ytt_mean_OCI > -7 & ytt_mean_OCI < 3 | ytt_mean_OCI == -1000),
+                 "taxid", file.path(fig_dir, "taxid_mean_OCI.jpeg"))
+plot_event_study(subset(lfs_sum_dist, ytt_mean_OCI > -7 & ytt_mean_OCI < 6 | ytt_mean_OCI == -1000), 
+                 subset(lfs_sum_dist, ytt_mean_OCI > -7 & ytt_mean_OCI < 6 | ytt_mean_OCI == -1000),
+                 "socinsur", file.path(fig_dir, "socinsur_mean_OCI.jpeg"))
 
 plot_event_study(default_f, lfs_sum_dist_f, "work", file.path(fig_dir, "work_mean_OCI_f.jpeg"))
 plot_event_study(default_f, lfs_sum_dist_f, "agri", file.path(fig_dir, "agri_mean_OCI_f.jpeg"))
